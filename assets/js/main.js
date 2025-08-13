dc18aeb2a9202eba2882fe7846d0c15ff610a6a0
Taylor Scott <65211220+t-z-scott@users.noreply.github.com>
2025-08-13 05:32:26 -0700

    navbar!!
    changelog:
    - created `main.js` and `navbar.css` for dropdown functionality
    - created and updated `navigation.html` to have the elements written out, not dynamically added. put this feature in roadmap for later?
    - slight text changes to `categories.html` and `tags.html`

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/* originally tested in my codepen: https://codepen.io/typestooloud/details/myeqmoY */
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function showDropdown() {
+  document.getElementById("posts-dropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("posts-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
